f948c4a73b9f4d715d4c50b05002c11f01127974
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2021-09-09 17:28:38 -0400

    [ADD] controller tags

--- src/modules/fuels/fuels.controller.ts
+++ src/modules/fuels/fuels.controller.ts
@@ -3,11 +3,13 @@ import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 import { QueryFuelPriceDto, ResponseFuelPriceDto } from './dtos';
 import { TransformInterceptor } from '@common/interceptors';
 import { FuelsService } from './fuels.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller({
   path: 'fuels',
   version: '1',
 })
+@ApiTags('Fuel Prices')
 export class FuelsController {
   constructor(private readonly fuelsService: FuelsService) {}
 
